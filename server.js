02e0d58791a3a985b4e18abc72c1fd29dfdc70b1
Sean P <perez.seanneptali.3@gmail.com>
2026-08-20 11:13:20 +0800

    Fix Render error

--- server.js
+++ server.js
@@ -39,8 +39,6 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
 }));
 
-// Handle preflight across all routes
-app.options('*', cors());
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
